a9a5f16ac610b98395d6134c7b97db1c81dbf0e4
Sander Demihhovski <demihhovski@mail.ru>
2020-09-10 17:17:04 +0300

    SliderStepper Done

--- DemihhovskiNewApp/DemihhovskiNewApp/SliderStepper.xaml.cs
+++ DemihhovskiNewApp/DemihhovskiNewApp/SliderStepper.xaml.cs
@@ -20,32 +20,39 @@ namespace DemihhovskiNewApp
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
-            Editor1.FontSize = Slider1.Value;
+        }
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (sender == Slider1)
+            {
+                redLabel.Text = String.Format("{0:X2}", (int)e.NewValue);
+            }
+            else if (sender == Slider2)
+            {
+                redLabel.Text = String.Format("{0:X2}", (int)e.NewValue);
+            }
+            else if (sender == Slider3)
+            {
+                redLabel.Text = String.Format("{0:X2}", (int)e.NewValue);
+            }
+            box.Color = System.Drawing.Color.FromArgb((int)Slider1.Value,
+                (int)Slider2.Value,
+                (int)Slider3.Value);
         }
 
-        private void Slider1_ValueChanged(object sender, ValueChangedEventArgs e)
+        private void Stepper1_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             if (e.NewValue > 0)
             {
-                Editor1.FontSize = e.NewValue;
+                Slider1.Value = e.NewValue;
             }
         }
-
-        private void Slider2_ValueChanged(object sender, ValueChangedEventArgs e)
+        Random clr = new Random();
+        private async void Rand_Clicked(object sender, EventArgs e)
         {
-            if (e.NewValue > 0)
-            {
-                Editor1.FontSize = e.NewValue;
-            }
-        }
-
-        private void Slider3_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            if (e.NewValue > 0)
-            {
-                Editor1.FontSize = e.NewValue;
-            }
+            Slider1.Value = clr.Next(0, 255);
+            Slider2.Value = clr.Next(0, 255);
+            Slider3.Value = clr.Next(0, 255);
         }
     }
 }
